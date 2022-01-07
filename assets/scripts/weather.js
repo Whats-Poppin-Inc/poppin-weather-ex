d06960e8054978383995d18aa7a33d8cbfec7b0a
Miles Weber <milesweber@comcast.net>
2022-01-07 14:25:55 -0500

    Added Real Feel to weather report

--- assets/scripts/weather.js
+++ assets/scripts/weather.js
@@ -4,14 +4,20 @@ var long = '';
 
 $.getJSON("env.json", function(json) {
     apikey = json.API_KEY; 
- });
+});
+
 async function getWeather(){
     getLocation();
     let url = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+long+'&units=metric&appid='+apikey;
     let response = await fetch(url);
     let data = await response.json();
+
+    //log results
     console.log(data);
+
+    //if there is no error get the weather
     document.getElementById('temp').innerHTML = data.current.temp + '&deg;C';
+    document.getElementById('feelslike').innerHTML = data.current.feels_like + '&deg;C';
 }
 
 function getLocation(){
